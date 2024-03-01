626e828d34231482e7d18669b0ec97db44929947
takuvuma <takuvuma@gmail.com>
2024-03-01 16:46:32 -0600

    noq

---
+++ EduPulse/views/HomeView /Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home .swift
+//  EduPulse
+//
+//  Created by Takudzwa Vuma on 2/23/24.
+//
+
+import SwiftUI
+
+struct Home_: View {
+    
+    
+    var body: some View {
+        
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+        }
+    }
+}
+
+#Preview {
+    Home_()
+}
